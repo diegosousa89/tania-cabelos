e968552fc2ae94feeff12f615895c027c4ae558b
diegosousa89 <diegosousa899@gmail.com>
2025-09-25 13:23:25 -0300

    adicionando outras telas linkagem

--- tania/home/urls.py
+++ tania/home/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), # A URL '' (vazia) chama a função 'home' em views.py
+    path('agenda-admin.html', views.agenda_admin, name='agenda_admin'),
+    path('agenda-cliente.html', views.agenda_cliente, name='agenda_cliente'),
+    path('perfil-admin.html', views.perfil_adm, name='perfil_adm'), 
+    path('perfil-cliente.html', views.perfil_cliente, name='perfil_cliente'),
+    path('login-user-admin.html', views.login, name='login'),
+  
+
 ]
--- tania/home/views.py
+++ tania/home/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')    
+    return render(request, 'index.html')  
+
+def login(request):
+    return render(request, 'login-user-admin.html')  
+
+def perfil_adm(request):
+    return render(request, 'pagina-admin.html') 
+
+def perfil_cliente(request):
+    return render(request, 'perfil-cliente.html')
+
+def agenda_cliente(request):
+    return render(request, 'agenda-cliente.html')
+
+def agenda_admin(request):
+    return render(request, 'agenda-admin.html')
